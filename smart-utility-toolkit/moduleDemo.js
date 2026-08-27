5ce72b3ef0299d4491cec5ab2ddb1420ae1b1b56
srishti-bh31 <srishtibh31@gmail.com>
2026-08-27 20:53:53 +0530

    Add files via upload

---
+++ smart-utility-toolkit/moduleDemo.js
@@ -0,0 +1,8 @@
+const isEven = require("./isEven");
+
+console.log("Checking numbers...");
+
+console.log("10 is even:", isEven(10));
+console.log("7 is even:", isEven(7));
+console.log("24 is even:", isEven(24));
+console.log("15 is even:", isEven(15));
